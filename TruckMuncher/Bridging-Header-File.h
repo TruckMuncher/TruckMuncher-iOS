8573dced68f654dc18873b3ac28ee5afdf2c248a
Josh Ault <josh.ault3@gmail.com>
2014-09-19 21:23:19 -0500

    renamed bridging header file to be more generic since more than just facebook will go in here eventually. fixed auto layout

---
+++ TruckMuncher/Bridging-Header-File.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header-File.h
+//  TruckMuncher
+//
+//  Created by Josh Ault on 9/19/14.
+//  Copyright (c) 2014 TruckMuncher. All rights reserved.
+//
+
+#ifndef TruckMuncher_Bridging_Header_File_h
+#define TruckMuncher_Bridging_Header_File_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
